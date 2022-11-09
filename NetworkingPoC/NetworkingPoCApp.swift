7195f340eb18e1c46469f10a3a4cdccbdba7b7eb
Frank Solleveld <fsolleveld@outlook.com>
2022-11-09 10:28:38 +0100

    Initial Commit

---
+++ NetworkingPoC/NetworkingPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingPoCApp.swift
+//  NetworkingPoC
+//
+//  Created by Frank Solleveld on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
