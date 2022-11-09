5ce2f3d33c39f02aeda118edadce62281d2eae35
Frank Solleveld <fsolleveld@outlook.com>
2022-11-09 13:35:34 +0100

    NetworkingPoC

--- NetworkingPoCTests/NetworkingPoCTests.swift
+++ NetworkingPoCTests/NetworkingPoCTests.swift
@@ -9,28 +9,25 @@ import XCTest
 @testable import NetworkingPoC
 
 final class NetworkingPoCTests: XCTestCase {
+    func test_UrlProtocolMock() {
+        let url = URL(string: "https://github.com")
+        let data = Data()
+        let response = HTTPURLResponse(url: URL(string: "https://github.com")!, statusCode: 200, httpVersion: nil, headerFields: nil)
+        URLProtocolMock.mockURLs = [url: (nil, data, response)]
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let sessionConfiguration = URLSessionConfiguration.ephemeral
+        sessionConfiguration.protocolClasses = [URLProtocolMock.self]
+        let mockSession = URLSession(configuration: sessionConfiguration)
+
+        let task = mockSession.dataTask(with: url!, completionHandler: { (data, response, error) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(data)
+            guard let httpResponse = response as? HTTPURLResponse else {
+                XCTFail("unexpected response")
+                return
+            }
+            XCTAssertEqual(200, httpResponse.statusCode)
+        })
+        task.resume()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
